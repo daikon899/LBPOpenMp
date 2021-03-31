2c92901feb6b9f558168b6b852d780bbfd7710ae
MarcoSolarino <marcosolarino@live.it>
2021-03-31 16:56:47 +0200

    - added CmakeList - added missed omp parallel

--- LocalBinaryPattern.cpp
+++ LocalBinaryPattern.cpp
@@ -7,29 +7,33 @@ Mat localBinaryPattern(Mat& imgIn) {
 
     copyMakeBorder(imgIn, imgIn, 1, 1, 1, 1, BORDER_CONSTANT, 0);
 
-    #pragma omp for collapse(2)
-    for (int i = 1; i < imgIn.rows - 1; i++) {
-        for (int j = 1; j < imgIn.cols - 1; j++) {
+    #pragma omp parallel shared(imgIn, weights)
+    {
+        #pragma omp for collapse(2) schedule(static)
+                for (int i = 1; i < imgIn.rows - 1; i++) {
+                    for (int j = 1; j < imgIn.cols - 1; j++) {
 
-            int neighbors[8];
-            neighbors[0] = imgIn.at<uchar>(i - 1, j - 1);
-            neighbors[1] = imgIn.at<uchar>(i - 1, j);
-            neighbors[2] = imgIn.at<uchar>(i - 1, j + 1);
-            neighbors[3] = imgIn.at<uchar>(i, j + 1);
-            neighbors[4] = imgIn.at<uchar>(i + 1, j + 1);
-            neighbors[5] = imgIn.at<uchar>(i + 1, j);
-            neighbors[6] = imgIn.at<uchar>(i + 1, j - 1);
-            neighbors[7] = imgIn.at<uchar>(i, j - 1);
+                        int neighbors[8];
+                        neighbors[0] = imgIn.at<uchar>(i - 1, j - 1);
+                        neighbors[1] = imgIn.at<uchar>(i - 1, j);
+                        neighbors[2] = imgIn.at<uchar>(i - 1, j + 1);
+                        neighbors[3] = imgIn.at<uchar>(i, j + 1);
+                        neighbors[4] = imgIn.at<uchar>(i + 1, j + 1);
+                        neighbors[5] = imgIn.at<uchar>(i + 1, j);
+                        neighbors[6] = imgIn.at<uchar>(i + 1, j - 1);
+                        neighbors[7] = imgIn.at<uchar>(i, j - 1);
 
-            int oldVal = imgIn.at<uchar>(i, j);
+                        int oldVal = imgIn.at<uchar>(i, j);
 
-            int newVal = 0;
-            for (int k = 0; k < 8; k++) {
-                if (neighbors[k] >= oldVal )
-                    newVal += weights[k];
-            }
-            imgOut.at<uchar>(i - 1, j - 1) = newVal;
-        }
+                        int newVal = 0;
+                        for (int k = 0; k < 8; k++) {
+                            if (neighbors[k] >= oldVal )
+                                newVal += weights[k];
+                        }
+                        imgOut.at<uchar>(i - 1, j - 1) = newVal;
+                    }
+                }
+
     }
 
     return imgOut;
