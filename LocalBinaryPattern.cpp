2d42d974ffb3613279343ed3e4c31de23c51244a
Simone <simone.pezzulla@stud.unifi.it>
2021-03-31 14:12:49 +0200

    simplified code

--- LocalBinaryPattern.cpp
+++ LocalBinaryPattern.cpp
@@ -2,7 +2,7 @@
 
 
 Mat localBinaryPattern(Mat& imgIn) {
-    const int *weights = new int[8]{1, 2, 4, 8, 16, 32, 64, 128};
+    int weights[8] = {1, 2, 4, 8, 16, 32, 64, 128};
     Mat imgOut = Mat::zeros(imgIn.rows, imgIn.cols, CV_8UC1);
 
     copyMakeBorder(imgIn, imgIn, 1, 1, 1, 1, BORDER_CONSTANT, 0);
@@ -11,9 +11,7 @@ Mat localBinaryPattern(Mat& imgIn) {
     for (int i = 1; i < imgIn.rows - 1; i++) {
         for (int j = 1; j < imgIn.cols - 1; j++) {
 
-            // get neighbors
             int neighbors[8];
-
             neighbors[0] = imgIn.at<uchar>(i - 1, j - 1);
             neighbors[1] = imgIn.at<uchar>(i - 1, j);
             neighbors[2] = imgIn.at<uchar>(i - 1, j + 1);
@@ -25,18 +23,9 @@ Mat localBinaryPattern(Mat& imgIn) {
 
             int currentPixelGS = imgIn.at<uchar>(i, j);
 
-            bool differences[8];
-
-            for (int k = 0; k < 8; k++) {
-                if (neighbors[k] >= currentPixelGS)
-                    differences[k] = true;
-                else
-                    differences[k] = false;
-            }
-
             int newVal = 0;
             for (int k = 0; k < 8; k++) {
-                if (differences[k])
+                if (neighbors[k] >= currentPixelGS )
                     newVal += weights[k];
             }
 
@@ -44,8 +33,5 @@ Mat localBinaryPattern(Mat& imgIn) {
 
         }
     }
-
-    delete[] weights;
-
     return imgOut;
 }
